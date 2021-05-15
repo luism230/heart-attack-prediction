67e9b81fe50ba0c445ce084beedda4b8465216ed
Luis Ramirez Mendoza <luism.ramirez23@gmail.com>
2021-05-14 22:03:13 -0700

    Final Commit

--- find-best-model.py
+++ find-best-model.py
@@ -1,10 +1,3 @@
-import sys
-import scipy
-import numpy
-import matplotlib
-import pandas
-import sklearn
-
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
@@ -17,33 +10,47 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import BaggingClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.linear_model import LinearRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-from sklearn import preprocessing
 
+#Load Dataset
 file_name = "heart.csv"
-names = ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'oldpeak', 'slp', 'caa', 'thall', 'output']
-dataset = read_csv(file_name, names=names)
+categories = ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'oldpeak', 'slp', 'caa', 'thall', 'output']
+dataset = read_csv(file_name, names=categories)
 
-o2_sat = 
+#Observe Dataset
+print(dataset.shape)
+print(dataset.head(20))
+print(dataset.describe())
+print(dataset.groupby('output').size())
 
+input('')
+
+#Separate into X and Y values
 array = dataset.values
-x = array[:,0:-1]
-y = array[:,-1]
+x = array[1:,0:-1]
+y = array[1:,-1]
 
-x = preprocessing.scale(x)
+#Separate Data into Training and Validation sets
+X_train, X_validation, Y_train, Y_validation = train_test_split(x, y, test_size = 0.10, random_state = 1)
 
-X_train, X_validation, Y_train, Y_validation = train_test_split(x, y, test_size = 0.20, random_state = 1)
 
+#Load Models to test
 models = []
 models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
 models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
+models.append(('KNC', KNeighborsClassifier()))
+models.append(('DTC', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
+models.append(('SVC', SVC(gamma='auto')))
+models.append(('QDA', QuadraticDiscriminantAnalysis()))
+models.append(('BC', BaggingClassifier()))
 
+#Cross Validation to Evaluate Accuracy of each Model
 results = []
 names = []
 for name, model in models:
@@ -52,13 +59,19 @@ for name, model in models:
     results.append(cv_results)
     names.append(name)
     print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+input('')
 
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
 
+#Test and Validate Chosen Model
 model = LogisticRegression(solver='liblinear', multi_class='ovr')
 model.fit(X_train, Y_train)
 predictions = model.predict(X_validation)
 
-#print(predictions)
+#Results
+print(predictions)
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
--- heart-attack-predictor.py
+++ heart-attack-predictor.py
@@ -1,82 +1,28 @@
-import sys
-import scipy
-import numpy
-import matplotlib
-import pandas
-import sklearn
-
 from pandas import read_csv
-from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-from sklearn import preprocessing
 
+#Load Dataset
 file_name = "heart.csv"
-names = ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'oldpeak', 'slp', 'caa', 'thall', 'output']
-dataset = read_csv(file_name, names=names)
+categories = ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'oldpeak', 'slp', 'caa', 'thall', 'output']
+dataset = read_csv(file_name, names=categories)
 
-
-print(dataset.shape)
-
-print(dataset.head(20))
-
-print(dataset.describe())
-print(dataset.groupby('output').size())
-
-
-
+#Separate into X and Y values
 array = dataset.values
-x = array[:,0:-1]
-y = array[:,-1]
+x = array[1:,0:-1]
+y = array[1:,-1]
 
-x = preprocessing.scale(x)
-
-X_train, X_validation, Y_train, Y_validation = train_test_split(x, y, test_size = 0.20, random_state = 1)
-
+#Separate into Training and Validation Data
+X_train, X_validation, Y_train, Y_validation = train_test_split(x, y, test_size = 0.10, random_state = 1)
 
 
-models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
-
-results = []
-names = []
-for name, model in models:
-    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
-    results.append(cv_results)
-    names.append(name)
-    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
-
-pyplot.boxplot(results, labels=names)
-pyplot.title('Algorithm Comparison')
-#pyplot.show()
-
+#Fit the Model and Make Predictions
 model = LogisticRegression(solver='liblinear', multi_class='ovr')
 model.fit(X_train, Y_train)
-predictions = model.predict(X_validation)
 
-#print(predictions)
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
-
-#patient = []
+#Ask for Patient Info
+patient = []
 patient.append(input("Enter Age of the Patient"))
 patient.append(input("Sex:"))
 patient.append(input("cp:"))
@@ -91,16 +37,17 @@ patient.append(input("slp:"))
 patient.append(input("caa:"))
 patient.append(input("thall:"))
 
+#Convert All Values into Floats
 patient = list(map(float, patient))
 patient = [patient]
 
+#Make Prediction and Print Result
 prediction = model.predict(patient)
 if prediction == 1:
     print("This patient has more chance of experiencing a heart attack")
 else:
     print("This patient has less chance of experiencing  a heart attack")
-#print(x)
-#print(y)
+
 
 
 
